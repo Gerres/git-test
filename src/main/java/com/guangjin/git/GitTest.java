3236736633d60dd309e25a57a5e0489905261f37
Layne <Layne@qq.com>
2022-04-05 19:40:53 +0800

    first commit

---
+++ src/main/java/com/guangjin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.guangjin.git;
+
+/**
+ * @author jbb~
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
